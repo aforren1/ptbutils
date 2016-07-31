c8ab10c28cfa1d82fcb23b04fd63783dcb4d9d59
Alex Forrence <aforren1@jhu.edu>
2016-07-31 18:30:13 -0400

    Added click train utility function

---
+++ GenClick.m
@@ -0,0 +1,29 @@
+function out = GenClick(click_freq, inter_click_interval, num_clicks)
+% GENCLICK Generates a click train at a sampling rate of 44.1 kHz
+%
+% GenClick(click_freq, inter_click_interval, num_clicks);
+%
+% Use 1046 (high C) for click freq
+% I'll add (0.5 - 1/2*length(beep)) seconds to the front of the click train,
+% to make lining up easier(?). The middle of the first beep would then occur at 0.5s
+%
+% Inputs:
+%     click_freq - Frequency of the beep (Hz)
+%     inter_click_interval - Period of click train (in seconds)
+%     num_clicks - Number of clicks in the train (3 or 4, usually...)
+    Fs = 44100;
+    click_dur = 0.05;
+    beep = MakeBeep(click_freq, click_dur, Fs);
+    space = zeros(1, (inter_click_interval * Fs) - length(beep));
+    ramp = linspace(0, 1, .1*length(beep));
+    rampdown = linspace(1, 0, .1*length(beep));
+    beep(1:length(ramp)) = beep(1:length(ramp)) .* ramp;
+    beep(end - length(ramp) + 1:end) = beep(end - length(ramp) + 1:end) .* rampdown;
+
+
+    out = zeros(1, Fs*0.5 - (length(beep)/2));
+
+    for ii = 1:num_clicks
+        out = [out, beep, space];
+    end
+end
