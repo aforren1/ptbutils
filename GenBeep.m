4e33928c898a4643f00b9635f747a75588d2e578
aforren1 <aforren1@jhu.edu>
2016-08-04 15:47:53 -0400

    Typo

--- GenBeep.m
+++ GenBeep.m
@@ -9,7 +9,7 @@ function out = GenBeep(beep_freq)
     Fs = 44100;
     beep_dur = 0.04;
     beep = MakeBeep(beep_freq, beep_dur, Fs);
-    rampup = linsapce(0, 1, 0.1 * length(beep));
+    rampup = linspace(0, 1, 0.1 * length(beep));
     rampdown = fliplr(rampup);
 
     beep(1:length(rampup)) = beep(1:length(rampup)) .* ramp;
